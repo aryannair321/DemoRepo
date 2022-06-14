8d51ce785bea64296c378512100b82f2e3798c2d
Aryan Nair <aryan.nair@BLRETV-JC2K52.local>
2022-06-14 16:03:29 +0530

    second commit

--- Demo.java
+++ Demo.java
@@ -1,9 +1,5 @@
-class Demo
-{
-    // Your program begins with a call to main().
-    // Prints "Hello, World" to the terminal window.
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
